d3f028560af9fa3a76b62d9abc99b15b08a20eba
Kevin Bruner-Prime <kevin.a.bruner@gmail.com>
2026-09-22 22:30:43 -0400

    new download plus enabling tautulli service in first-boot script

--- roles/first_boot/templates/kevin-bootstrap.sh
+++ roles/first_boot/templates/kevin-bootstrap.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+
+# Installs latest Plex package
 dpkg -i /home/kevin/plexmediaserver.deb 
+
+# Configures the Tautulli service (which is installed on a persistent NFS share)
+cp /opt/Tautulli/init-scripts/init.systemd /etc/systemd/system/tautulli.service
+systemctl enable tautulli.service
+systemctl daemon-reload
+systemctl start tautulli.service
